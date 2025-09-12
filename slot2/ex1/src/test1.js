fa52141b22ea2a86277e6ab5982c12b84cfadad1
1503 <quangncde170490@fpt.edu.vn>
2025-09-12 11:29:26 +0700

    Add test1.js to slot2/ex1

---
+++ slot2/ex1/src/test1.js
@@ -0,0 +1,31 @@
+let add = (a, b) => a+b 
+console.log(add(2,3));
+
+let greet = (name, timeOfDay) => {
+    console.log(`Good ${timeOfDay}, ${name}!`);
+}
+greet("Alice", "morning");
+greet("Bob", "evening");
+
+let square = num =>{
+    return num * num;
+};
+
+console.log(square(5));
+console.log(square(8));
+
+let sayHello = () => {
+    console.log("Hello there!");
+}
+
+sayHello();
+
+let person = {
+    name: "John",
+    age: 30,
+    greet: function() {
+        console.log(`Hi, I'm ${this.name} and I'm ${this.age} years old.`);
+    }
+};
+
+person.greet();
